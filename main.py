0593176158c25f15ecd878223322853875cd1db1
come-bruneteau <comebruneteaupro@gmail.com>
2026-04-07 12:27:24 +0200

    feat: load all dataset

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from manager.dataset import Dataset
 
-Dataset(name="Sample Dataset", file_path="assets/lowbid_manche_demo.csv")
+assets = [
+    "assets/lowbid_manche_demo.csv",
+    "assets/lowbid_multi_manches_500x40.csv",
+    "assets/lowbid_stress_200k.csv"
+]
+
+datasets = []
+
+for file_path in assets:
+    datasets.append(Dataset(name="Sample Dataset", file_path=file_path))
+
+if len(datasets) == 0:
+    print("No datasets loaded.")
